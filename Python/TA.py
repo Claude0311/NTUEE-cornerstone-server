f054397474235750d4b36b91a3ae6b2fc26893fc
Lu Ying Hong <louis8683@gmail.com>
2021-05-05 18:46:42 +0800

    python files by Lu

--- Python/TA.py
+++ Python/TA.py
@@ -1,30 +1,27 @@
 import socketio
 import requests
 import sys
+import time
 
+from score import Socket
 
-ip = "http://192.168.50.83:3000"
+ip = "http://192.168.50.44:3000"
+# ip = "https://creative.ntuee.org"
+
 def stop_game():
-    g = requests.get(ip+"/game_status")
+    g = requests.get(ip + "/game_status")
     cur_team = g.json()['current_team']
     if(cur_team != None):
-        sio = socketio.Client()
-        sio.connect(ip)
-        sio.emit("stop_game")
-        sio.wait(3000)
-        sio.disconnect()
+        socket = Socket(ip)
+        socket.stop_game()
 
 def reset_game():
-    g = requests.get(ip+"/reset?pass=taonly")
+    g = requests.get(ip + "/reset?pass=taonly")
     print(g.json())
 
 if __name__ == '__main__':
     #stop_game()
-    try:
-        if(sys.argv[1] == 'stop'):
-            stop_game()
-        elif(sys.argv[1] == 'reset'):
-            reset_game()
-    except:
-        print("Error")
-        pass
+    if(sys.argv[1] == 'stop'):
+        stop_game()
+    elif(sys.argv[1] == 'reset'):
+        reset_game()
