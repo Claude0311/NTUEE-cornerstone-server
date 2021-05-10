0ab7e7df32d9ac15a354da093fa88ee5e9ceff33
MidLaneBro <chung.a40709@gmail.com>
2021-05-11 00:18:20 +0800

    final version

--- Python/score.py
+++ Python/score.py
@@ -127,7 +127,7 @@ class Socket(socketio.ClientNamespace):
 
 if __name__ == '__main__':
     import time
-    myScoreboard = Scoreboard(None,'Test2')
+    myScoreboard = Scoreboard(None,'Test')
     time.sleep(3)
     myScoreboard.add_UID("71A5261C")
     print(myScoreboard.getCurrentScore())
